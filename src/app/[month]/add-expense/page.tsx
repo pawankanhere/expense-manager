89fa02ec53665b5e1b20c401179f5e8c4e6f5529
Pawan Kanhere <yourpawankanhere1991@gmail.com>
2025-03-22 20:56:49 +0530

    style: added background color to add expense form

--- src/app/[month]/add-expense/page.tsx
+++ src/app/[month]/add-expense/page.tsx
@@ -3,9 +3,13 @@ import { AddExpenseForm } from "./add-expense-form"
 
 const AddExpense = async () => {
   return (
-    <div className="w-full 2xl:w-[520px]">
-      <h2 className="text-sm font-semibold text-gray-800 sm:text-base xl:text-lg xl:font-semibold">Add Transaction</h2>
-      <AddExpenseForm />
+    <div className="w-full bg-white border  rounded-xl shadow shadow-slate-200 p-4">
+      <div className="w-full 2xl:w-[520px] bg-white p-4">
+        <h2 className="text-sm font-semibold text-gray-800 sm:text-base xl:text-lg xl:font-semibold">
+          Add Transaction
+        </h2>
+        <AddExpenseForm />
+      </div>
     </div>
   )
 }
